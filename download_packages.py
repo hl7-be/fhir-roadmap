38e9e4852cbdeeb51cc2de46fdf02b7b4b76de00
Bart Decuypere (eHealth) <90335317+bdc-ehealth@users.noreply.github.com>
2022-05-05 19:34:49 +0200

    added download script + fixed some bugs

---
+++ download_packages.py
@@ -0,0 +1,29 @@
+import urllib.request 
+import shutil
+import os
+
+from AdvancedHTMLParser.Parser import AdvancedHTMLParser
+
+serverlocation = 'https://build.fhir.org/ig/hl7-be/'
+#igs = ['core','core-clinical','vaccination','allergy','lab']
+igs = []
+package = 'package.tgz'
+target = './test/packages'
+exclude = ['..']
+
+os.makedirs(target, exist_ok=True)
+urllib.request.urlretrieve(serverlocation, target +'/'+ 'index.html')
+parser = AdvancedHTMLParser()
+parser.parseFile(target +'/'+ 'index.html')
+anchors = parser.getElementsByTagName('a') 
+for anchor in anchors:
+    igs.append( anchor.getAttribute('href').split('/')[0])
+
+for ig in igs:
+    if ig not in exclude:
+        try:
+            print(ig)
+            urllib.request.urlretrieve(serverlocation +'/'+ig+'/'+package, target + '/'+ package)
+            shutil.unpack_archive(target + '/'+ package, target + '/' + ig)
+        except urllib.error.HTTPError:
+            print('Package not published')    
