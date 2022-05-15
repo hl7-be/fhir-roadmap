bda28f0157f9ac000011e8c36843df4442c86fe3
Bart Decuypere (eHealth) <90335317+bdc-ehealth@users.noreply.github.com>
2022-05-15 20:29:57 +0200

    Update dispatch.sh

--- dispatch.sh
+++ dispatch.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/bash
 # TODO: replace :token, :user, and :repo
-curl -H "Authorization: token $1" \
+echo $ACTION_TOKEN
+curl -H "Authorization: token $ACTION_TOKEN" \
     -H 'Accept: application/vnd.github.everest-preview+json' \
     "https://api.github.com/repos/hl7-be/fhir-roadmap/dispatches" \
     -d '{"event_type": "update", "client_payload": {"foo": "bar"}}'
